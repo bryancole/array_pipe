ba88f16c035d39d7ef72af5a10fd42bbe345fd87
bryancole <bryancole.cam@gmail.com>
2011-02-09 08:16:30 +0000

    all three tests run

--- src/test.py
+++ src/test.py
@@ -14,7 +14,7 @@ def operation(conn):
     try:
         while True:
             data = conn.recv()
-            a = data[0]+1
+            #a = data[0]+1
             #del data
     except EOFError:
         return
@@ -41,7 +41,7 @@ def operation2(conn):
             if data is None:
                 break
             #print data[:5]
-            a = data[0]+1
+            #a = data[0]+1
     except EOFError:
         return
 
@@ -62,25 +62,32 @@ def test2():
         
 def operation3(conn):
     try:
+        ct = 0
         buf = numpy.zeros(shape=(datasize,), dtype=numpy.double)
+        size = buf.nbytes
         while True:
-            conn.recv_bytes_into(buf)
-            a = buf[0] + 1
+            n = conn.recv_bytes_into(buf)
+            if n < size:
+                break
+            #a = buf[0] + 1
+            ct += 1
     except EOFError:
-        return
+        pass
+    print "total3", ct
     
 def test3():
     receiver, sender = Pipe(False)
     
     p = Process(target=operation3, args=(receiver,))
     p.start()
-    time.sleep(0.1)
     data = numpy.linspace(0,1,datasize)
     try:
         for i in xrange(10000):
             sender.send_bytes(data)
     finally:
+        sender.send("\z")
         sender.close()    
+        receiver.close()
         p.join()
 
 if __name__=="__main__":
@@ -88,6 +95,9 @@ if __name__=="__main__":
     start = time.time()
     test1()
     print "test1 took",time.time() - start
+    start = time.time()
+    test3()
+    print "test3 took",time.time() - start
     start = time.time()
     test2()
     print "test2 took",time.time() - start
